598042ad1ff9fe7ba9ac0dd9bf2271ea658b82d6
Marco van den Bout <m.vandenbout@inbrain.nl>
2018-05-31 15:20:42 +0200

    Logo, search form

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
     <div class="container" id="app">
         <header>
             <nav class="navbar navbar-expand-lg">
-                <span class="navbar-brand mb-0 h1">All Boots Dulli</span>
+                <span class="navbar-brand mb-0 h1">
+                    <div class="logo"></div>
+                </span>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNavBar" aria-controls="mainNavBar" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon">
                         <i class="fas fa-bars"></i>
@@ -17,9 +19,13 @@
                             <router-link to="/about" class="nav-link">Contact</router-link>
                         </li>
                     </ul>
+                    <form class="form-inline my-2 my-lg-0 ml-auto">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
                 </div>
             </nav>
-            <nav class="navbar navbar-expand-lg">
+            <nav class="navbar navbar-expand-lg bg-primary">
                 <router-link to="/" class="navbar-brand">
                     <i class="fas fa-home"></i>
                 </router-link>
@@ -60,5 +66,14 @@ export default {
 body {
     background-image: url('./assets/background.jpg');
     background-size: cover;
+}
+
+.logo {
+    background-image: url('./assets/logo.png');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: contain;
+    height: 100px;
+    width: 300px;
 }
 </style>
