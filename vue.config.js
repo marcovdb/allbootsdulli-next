237ff4dc1878cc9c3d4f1867df916fe7598d9f99
Marco van den Bout <m.vandenbout@inbrain.nl>
2018-06-23 11:34:05 +0200

    Strip Moment.js locales from bundle

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
+const webpack = require('webpack')
+
 module.exports = {
     productionSourceMap: false,
     devServer: {
         proxy: 'http://allbootsdulli.ubuntu.local'
+    },
+    configureWebpack: {
+        plugins: [
+            // Ignore all locale files of moment.js
+            new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+          ]
     }
 }
